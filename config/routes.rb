4a778b27b5ba93442472a21822bafa53106e929c
EmmanuelVillar <e.villarbarrera@gmail.com>
2018-10-20 13:04:36 -0300

    Actualizacion ProyectoPinteres

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/team_mates'
   get 'pages/index'
+  get 'pages/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
