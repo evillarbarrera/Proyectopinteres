e87c690881858d7e740e3145caa7168ece60edbf
evillarbarrera <e.villarbarrera@gmai.com>
2018-10-27 12:29:38 -0300

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
   get 'pages/team_mates'
   get 'pages/index'
   get 'pages/home'
+
+  root 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
