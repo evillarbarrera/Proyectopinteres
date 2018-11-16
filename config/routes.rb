89f9f4911899828a1b057734a0862692882190d7
evillarbarrera <e.villarbarrera@gmai.com>
2018-11-15 22:26:12 -0300

    change view pins

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'pages/index'
   get 'pages/home'
 
-  root 'pages#home'
+  root 'pins#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
